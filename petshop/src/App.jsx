63dc43f986e723bdc4598d648279691151883680
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-09-08 21:56:02 -0300

    Criando routes paras as páginas

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import "./assets/css/base/base.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/Pagina404";
 
 function App() {
   return (
-  <>
-
-  </>
-  )
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/sobre">
+            <Sobre/>
+          </Route>
+          <Route path="*">
+            <Pagina404/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
