5cc8b6e1165dbc03223910aca072a309bbfc6d17
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-09-09 19:16:37 -0300

    Hook useHistory
    1- Importando o hook
    2- Atribuindo o método do hook a variável
    3- No useEffect, colocamos o método catch, com uma função anônima que ao não encontrar a rota, ela colocara a rota '/404', para o usuário ter o retorno visual que aquela rota que ele tentou acessar não existe

--- petshop/src/paginas/Post.jsx
+++ petshop/src/paginas/Post.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { busca } from '../api/api';
 import '../assets/css/post.css' 
 
 const Post = () => {
+    let history = useHistory()
     const { id } = useParams()
     const [post, setPost] = useState({});
 
     useEffect(() => {
-        busca(`/posts/${id}`,setPost)
+        busca(`/posts/${id}`,setPost).catch(() => {
+            history.push('/404')
+        })
     }, [id]);
     
     return ( 
