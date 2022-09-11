70b13d6c87f68cca753b4fb80d4083c87bd23d21
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-09-11 16:37:44 -0300

    Ajustes ortograficos
    1- Ajustes ortográficos , variáveis escritas erradas.
    2- Corrigindo o erro , o url estava no hook useParams, ao em vês, de está no useRouteMatch

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,44 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Route, useRouteMatch, Link } from "react-router-dom";
+import {
+  useParams,
+  Route,
+  useRouteMatch,
+  Link,
+  Switch,
+} from "react-router-dom";
 import ListaCategorias from "../components/ListaCategorias";
 import "../assets/css/blog.css";
 import ListaPost from "../components/ListaPost";
 import { busca } from "../api/api";
+import SubCategorias from '../paginas/SubCategorias'
 
 const Categoria = () => {
-  const [subCategorias, setSubCategorias] = useState([]);
-  const { url,id } = useParams();
-  const { path } = useRouteMatch();
+  const { id } = useParams();
 
+  const { url,path } = useRouteMatch();
+  
+  const [subcategorias, setSubCategorias] = useState([])
   useEffect(() => {
     busca(`/categorias/${id}`, (categoria) => {
-      setSubCategorias(categoria.subCategoria);
-    });
-  }, [id]);
+      setSubCategorias(categoria.subcategorias)
+    })
+  }, [id])
   return (
     <>
       <div className="container">
         <h2 className="titulo-pagina">Pet Notícias</h2>
       </div>
       <ListaCategorias />
-        
+
       <ul className="lista-categorias container flex">
-        {
-            subCategorias.map(subcategoria=>(
-                <li
-                className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
-                key={subcategoria}
-            >
-                <Link to={`${url}/${subcategoria}`}>
-                    {subcategoria}
-                </Link>
-            </li>
-            ))           
-        }
+        {subcategorias.map((subcategoria) => (
+          <li
+            className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+            key={subcategoria}
+          >
+            <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+          </li>
+        ))}
       </ul>
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-      </Route>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategorias />
+        </Route>
+      </Switch>
     </>
   );
 };
