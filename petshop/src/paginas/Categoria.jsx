94a6a06ce46200a5ef6d71b50b9cc97e1da83fb0
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-09-11 16:01:02 -0300

    Implementando a subcategorias
    1- Usamos o hook de de state para gerenciar o estado das subcategorias.
    2- No hook de useRouteMatch,, acrescentamos uma nova variável (url) para referência para a subcategorias.
    3- Hook useEffects para acessar o método busca acessando o array de categorias, e ao em vês de simplesmente usarmos o setSubcategoria, passamos uma função anônima já que precisamos de parâmetro para  modificar a subcategoria, neste caso o parâmetro é a categoria)
    4- Debaixo do componente de ListaCategorias, criamos uma lista, onde o itnes desta lista será feita po um map percorrendo a array das subcategorias,colocando o valor da key e o conteúdo como subcategoria. E no link a referencia dele é a url (pega de maneira dinâmica) , criando uma rota para a subcategorias

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import {useParams, Route, useRouteMatch}  from 'react-router-dom'
-import ListaCategorias from '../components/ListaCategorias'
-import '../assets/css/blog.css'
-import ListaPost from '../components/ListaPost'
+import React, { useState, useEffect } from "react";
+import { useParams, Route, useRouteMatch, Link } from "react-router-dom";
+import ListaCategorias from "../components/ListaCategorias";
+import "../assets/css/blog.css";
+import ListaPost from "../components/ListaPost";
+import { busca } from "../api/api";
 
 const Categoria = () => {
-    
-    const { id } = useParams()
-    const { path } = useRouteMatch()
+  const [subCategorias, setSubCategorias] = useState([]);
+  const { url,id } = useParams();
+  const { path } = useRouteMatch();
 
-    return(
-        <>
-        <div className="container">
-            <h2 className="titulo-pagina">Pet Notícias</h2>
-        </div>
-        <ListaCategorias/>
-        <Route exact path={`${path}/`}>
-            <ListaPost url={`/posts?categoria=${id}`}/>
-        </Route>
-        </>
-    )
-}
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subCategoria);
+    });
+  }, [id]);
+  return (
+    <>
+      <div className="container">
+        <h2 className="titulo-pagina">Pet Notícias</h2>
+      </div>
+      <ListaCategorias />
+        
+      <ul className="lista-categorias container flex">
+        {
+            subCategorias.map(subcategoria=>(
+                <li
+                className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+                key={subcategoria}
+            >
+                <Link to={`${url}/${subcategoria}`}>
+                    {subcategoria}
+                </Link>
+            </li>
+            ))           
+        }
+      </ul>
+      <Route exact path={`${path}/`}>
+        <ListaPost url={`/posts?categoria=${id}`} />
+      </Route>
+    </>
+  );
+};
 
-export default Categoria
+export default Categoria;
